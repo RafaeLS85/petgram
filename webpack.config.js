9fb9af68bd815d0c3d7e077cb8cbac9d4f78bca9
Rafael Olivay <rafaelsebastian@live.com.ar>
2022-08-09 21:21:04 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
